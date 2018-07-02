713e55e57aa31a1e475de4d6d727bdfd5a966332
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-02 06:04:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
 if(array[index] == "" || array[index] == " ") {
-  
+
+return false
 } else {
-  
+  return true
 }
 end
